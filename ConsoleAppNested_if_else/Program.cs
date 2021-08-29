faa4f954d16e33cd99ffad50890c29ff3459bf38
niles <niles@LAPTOP-3K24CQ1R>
2021-08-29 22:22:29 +0530

    Add project files.

---
+++ ConsoleAppNested_if_else/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppNested_if_else
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+          int firstno = 4, secondno = -2, thirdno = 10;
+            if(firstno>secondno)
+            {
+                if (firstno > thirdno)
+                {
+                    Console.WriteLine("{0} is the largest ", firstno);
+                }
+                else
+                {
+                    Console.WriteLine("{0} is the largest ", thirdno);
+                }
+            }
+            else
+            {
+                if(secondno>thirdno)
+                {
+                    Console.WriteLine("{0} is the largest ", secondno);
+                }
+                else
+                {
+                    Console.WriteLine("{0} is the largest ", thirdno);
+                }
+            }
+
+        }
+    }
+}
